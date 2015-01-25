0b4daaaa1a8db68f6a80fa6235f768834f225d09
AGHILLS <alan.hills.1978@gmail.com>
2015-01-25 21:39:41 +0000

    some text

--- cachematrix.R
+++ cachematrix.R
@@ -17,7 +17,7 @@ makeCacheMatrix <- function(x = matrix()) {
     inv <- NULL
     set <- function(y) {
         x <<- y
-        inv <<- NULL  #sets the inv to NULL on setting a new value
+        inv <<- NULL  #sets inv to NULL on setting a new value
     }
     get <- function() x
     setinverse <- function(inverse) inv <<- inverse
